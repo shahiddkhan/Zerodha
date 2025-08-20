b49e163a6b4fba07b65e5ad68d8126fd566ce990
shahiddkhan <shahidkhan176aa@gmail.com>
2025-08-20 23:10:07 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
